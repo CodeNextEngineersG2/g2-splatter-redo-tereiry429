7ec70a4933bab7cd4411d42229c2d6e226d2afcc
Student <student@Admins-MacBook-Air.local>
2018-03-10 11:29:41 -0800

    created splatter project

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-// Declare and initialize global variables
+// Declare global variables
 var canvas;
 var clearButton;
+var spread;
+vae slider;
 
 function setup() {
   canvas = createCanvas(500, 400);
   canvas.parent("#canvas-area");
-  canvas.mousePressed(drawEllipse);
+  canvas.mousePressed(drawSplatter);
   clearButton = select("#clear-button");
   clearButton.mousePressed(clearScreen);
+slider = select("#slider");
+slider.input(updateSpread);
   background(65, 60, 88);
+  //spread = 50;
+  updateSpread();
+}
+
+fuction updateSpread () {
+  spread = slider.value();
 }
 
 function drawEllipse() {
-  fill("#ff0000");
+fill("#ff0000");
   noStroke();
   ellipse(mouseX, mouseY, 100, 100);
 }
+function drawSplatter() {
+   fill("#04e889");
+  var randomNum = random(10, 15);
+  for(var i = 0; i < randomNum; i++) {
+     var randomSize = random(5, 15);
+     var x = random(mouseX - spread, mouseX + spread);
+     var y = random(mouseY - spread, mouseY + spread);
+  ellipse(x, y, randomSize,randomSize);
+  }
+}
+
+
 
 function clearScreen() {
   background(65, 60, 88);
